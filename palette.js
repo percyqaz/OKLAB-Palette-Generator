28f5a7cee29292a4b189fcfc90916c8fb2b81130
Percy Qaz <percyqaz@yahoo.co.uk>
2024-12-11 17:12:47 +0000

    Extend tool for more flexibility - Sliders give you finer control over palette instead of random - Changed "nearest color to palette" behaviour + can upload any grayscale image - Shell script to host the site locally

--- palette.js
+++ palette.js
@@ -1,7 +1,17 @@
+const imageSelect = document.getElementById('image_select');
+const imageInput = document.getElementById("image_input");
+const imageOutput = document.getElementById("image_output");
+
 var numColors = document.getElementById("numColors");
-var mode = document.getElementById("mode")
-var numColorsOP = document.getElementById("numColorsOP")
-var p1 = document.getElementById("p1") 
+var lightI = document.getElementById("lightI");
+var lightC = document.getElementById("lightC");
+var hueI = document.getElementById("hueI");
+var hueC = document.getElementById("hueC");
+var chromaI = document.getElementById("chromaI");
+var chromaC = document.getElementById("chromaC");
+var power = document.getElementById("power");
+var numColorsOP = document.getElementById("numColorsOP");
+var p1 = document.getElementById("p1");
 var palName = document.getElementById("palName");
 var bayer4x4 = [
     [  0, 8,  2, 10 ],
@@ -20,145 +30,116 @@ function rgbToHex(c) {
 }
 
 numColors.oninput = function() {
-    numColorsOP.innerHTML = "Number of Colors:"+this.value 
+    numColorsOP.innerHTML = "Number of Colors:" + this.value 
 }
-
-$('body').keyup(function(e){
-    if(e.keyCode == 32){
-        generatePalette()
-    }
- });
 
 function lerp( a, b, alpha ) {
     return a + alpha * ( b - a )
 }
 
-function parse(data){
-    palName.innerHTML = data.paletteTitle
-}
-
-function getPaletteName(colors){
-    vales = ""
-    for(let i = 0; i < colors.length-1; i++){
-        vales += colors[i]+","
-    }
-    vales += colors[colors.length-1]
-
-    $.ajax({
-        url: "https://api.color.pizza/v1/?values="+vales,
-        type: "get",
-        dataType: "json",
-        success: function (data) {
-            parse(data)
-        }
-    });
-}
-
-function getClosestColor(curColor, colors){
-    var dist = Infinity
-    var distInd = 0
-    for(let c = 0; c < colors.length; c++){
-        palCol = colors[c]
-        curDist = Math.sqrt(Math.pow(palCol[0]-curColor[0], 2)+Math.pow(palCol[1]-curColor[1], 2)+Math.pow(palCol[2]-curColor[2], 2))
-        if(curDist < dist){
-            dist = curDist
-            distInd = c
-        }
-    }
-    return colors[distInd]
+function getPaletteColor(level, dither, colors){
+	const ind = Math.floor((level + dither) * colors.length);
+    const c = Math.min(colors.length - 1, ind)
+    return colors[c];
 }
 
 function quantize(colors) {
-    const canvas = document.getElementById("sample")
-    const ctx = canvas.getContext("2d")
-    const img = document.getElementById("sampleimg")
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-    var imagedata = ctx.getImageData(0,0, canvas.width, canvas.height)
+    var imagedata = imageInput.getContext("2d", { willReadFrequently: true }).getImageData(0,0, imageInput.width, imageInput.height)
+	const pow = lerp(1, 8, power.value * 0.01)
     
-    for (var i = 0; i <= imagedata.data.length; i += 4) {
-        var x = i / 4 % canvas.width;
-        var y = Math.floor(i / 4 / canvas.width);
-        var curColor = [
-            imagedata.data[i+0] + (255/numColors.value)*(bayer4x4[x % 4][y % 4] * 1/16),
-            imagedata.data[i+1] + (255/numColors.value)*(bayer4x4[x % 4][y % 4] * 1/16), 
-            imagedata.data[i+2] + (255/numColors.value)*(bayer4x4[x % 4][y % 4] * 1/16), 
-        ]
-        var nurColor = [
-            imagedata.data[i+0],
-            imagedata.data[i+1], 
-            imagedata.data[i+2], 
-        ] 
-        getColor = getClosestColor(curColor, colors)
+    try {
+        for (var i = 0; i <= imagedata.data.length; i += 4) {
+            var x = i / 4 % imageInput.width;
+            var y = Math.floor(i / 4 / imageInput.width);
+            var level = (imagedata.data[i+0] / 255) ** pow // red channel - input image must be grayscale!
+			var dither = (1/numColors.value)*(bayer4x4[x % 4][y % 4] * 1/16)
+            getColor = getPaletteColor(level, dither, colors)
 
-        imagedata.data[i+0] = getColor[0]
-        imagedata.data[i+1] = getColor[1]
-        imagedata.data[i+2] = getColor[2]
+            imagedata.data[i+0] = getColor[0]
+            imagedata.data[i+1] = getColor[1]
+            imagedata.data[i+2] = getColor[2]
+        }
     }
-    ctx.putImageData(imagedata,0,0)
+    catch(e)
+    {
+        console.log(e);
+    }
+    imageOutput.getContext("2d").putImageData(imagedata,0,0)
 };
 
-function generatePalette(){
-    p1.innerHTML = ""
-    colors = []
-    colorsRGB = []
+function generatePalette() {
+	document.body.style.cursor = 'wait';
+    p1.innerHTML = "";
+    colors = [];
+    colorsRGB = [];
 
-    var baseL = lerp(.2,.7,Math.random())
-    var changeL = lerp(.2,1-baseL,Math.random())
-    var baseC = lerp(.05, .2, Math.random())
-    var changeC = lerp(.075,.2-baseC, Math.random())
-    var baseH = 2*Math.PI*Math.random()
-    var changeH = lerp(.3, 1, Math.random())
+    var baseL = lerp(0.0, 1.0, lightI.value * 0.01);
+    var changeL = lerp(-baseL, 1.0 - baseL, lightC.value * 0.01);
+    var baseC = lerp(0.0, 0.2, chromaI.value * 0.01);
+    var changeC = lerp(-baseC, 0.2 - baseC, chromaC.value * 0.01);
+    var baseH = 2 * Math.PI * hueI.value * 0.01;
+    var changeH = lerp(-1, 1, hueC.value * 0.01);
 
-    if(mode.value == "achromatic"){
-        baseC = 0
-        changeC = 0
-    }
-
-    for(let i = 0; i < numColors.value; i++){
-        var wheelItr = i / (numColors.value -1)
-        var offsetH = wheelItr * changeH * 2 * Math.PI + (Math.PI / 4)
-
-        if(mode.value == "monochrome") { offsetH *= 0 }
-        if(mode.value == "complementary") { offsetH *= .25 }
-        if(mode.value == "analgous") { offsetH *= 1/3 }
-        if(mode.value == "triadic") { offsetH *= 2/3 }
-        if(mode.value == "tetradic") { offsetH *= .75 }
+    for(let i = 0; i < numColors.value; i++) {
+        var wheelItr = i / (numColors.value -1);
         
-        var L = baseL + wheelItr * changeL
-        var H = baseH + offsetH
-        if (mode.value != "monochrome"){
-            var C = baseC
-        }
-        else{
-            var C = baseC + wheelItr * changeC
-        }
-        var colWrap = document.createElement("div")
-        colWrap.classList.add("color")
-        colWrap.style.backgroundColor = "oklch("+L+" "+C+" "+H+"rad)"
-        var colName = document.createElement("p")
+        var L = baseL + wheelItr * changeL;
+        var H = baseH + wheelItr * changeH * 2 * Math.PI;
+        var C = baseC + wheelItr * changeC;
+        var colWrap = document.createElement("div");
+        colWrap.classList.add("color");
+        colWrap.style.backgroundColor = "oklch("+L+" "+C+" "+H+"rad)";
+        var colName = document.createElement("p");
         var curCol = document.createElement("canvas");
-        curCol.id = "color"+i
-        curCol.width = "2"
-        curCol.height = "2"
-        const ctx = curCol.getContext("2d", {willReadFrequently: true})
-        ctx.fillStyle = "oklch("+L+" "+C+" "+H+"rad)"
-        ctx.fillRect(0, 0, curCol.width, curCol.height)
-        colName.innerHTML = "#"+rgbToHex(ctx.getImageData(0, 0, 1, 1).data)
-        if(L < .5){
+        curCol.id = "color"+i;
+        curCol.width = "2";
+        curCol.height = "2";
+        const ctx = curCol.getContext("2d", {willReadFrequently: true});
+        ctx.fillStyle = "oklch("+L+" "+C+" "+H+"rad)";
+        ctx.fillRect(0, 0, curCol.width, curCol.height);
+        colName.innerHTML = "#"+rgbToHex(ctx.getImageData(0, 0, 1, 1).data);
+        if(L < .5) {
             colName.style.color = "#ffffff";
         }
-        else{
+        else {
             colName.style.color = "#000000";
         }
         
-        colors.push(rgbToHex(ctx.getImageData(0, 0, 1, 1).data))
-        colorsRGB.push(ctx.getImageData(0, 0, 1, 1).data)
-        colWrap.appendChild(curCol)
-        colWrap.appendChild(colName)
-        p1.appendChild(colWrap)
+        colors.push(rgbToHex(ctx.getImageData(0, 0, 1, 1).data));
+        colorsRGB.push(ctx.getImageData(0, 0, 1, 1).data);
+        colWrap.appendChild(curCol);
+        colWrap.appendChild(colName);
+        p1.appendChild(colWrap);
     }
-    console.log(colorsRGB)
-    getPaletteName(colors)
-    quantize(colorsRGB)
+    quantize(colorsRGB);
+	document.body.style.cursor = 'default';
 }
 
+imageSelect.addEventListener('change', handleImageSelection);
+
+function handleImageSelection(event) {
+  const file = event.target.files[0];
+  if (file && file.type.startsWith('image/')) {
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      const img = new Image();
+      img.onload = function() {
+        imageInput.width = img.width;
+        imageInput.height = img.height;
+		imageOutput.width = img.width;
+		imageOutput.height = img.height;
+
+        const ctx = imageInput.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+	
+		generatePalette();
+      };
+      img.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  } else {
+    alert('Please select a valid image file.');
+  }
+}
